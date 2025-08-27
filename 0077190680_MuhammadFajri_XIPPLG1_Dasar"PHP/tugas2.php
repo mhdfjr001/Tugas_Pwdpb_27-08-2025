0765a283b06d5081fdfa39ae40e006f0d4d59b8a
mhdfjr001 <muhammadfajri2400@gmail.com>
2025-08-27 20:55:38 +0700

    Add files via upload

---
+++ 0077190680_MuhammadFajri_XIPPLG1_Dasar"PHP/tugas2.php
@@ -0,0 +1,9 @@
+// Ini adalah komentar satu baris <br>
+/* Ini adalah komentar yang bisa menjelaskan banyak baris */ <br>
+/**
+* Fungsi ini menampilkan pesan
+* @param string $message
+*/
+function displayMessage($message) {
+ echo $message;
+}
